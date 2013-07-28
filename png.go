5276177f9436387e16d8d888827a958bb1f6a02e
James Bowes <jbowes@repl.ca>
2013-07-28 19:39:07 -0300

    Scale image size based on text

--- png.go
+++ png.go
@@ -7,10 +7,12 @@ import (
 	"image/png"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 
 	"code.google.com/p/freetype-go/freetype"
+	"code.google.com/p/freetype-go/freetype/raster"
 	"code.google.com/p/freetype-go/freetype/truetype"
 )
 
@@ -43,17 +45,22 @@ var (
 		"blue":        Blue,
 	}
 
-	edge image.Image
+	shadow = color.RGBA{0, 0, 0, 125}
+
+	edge     image.Image
 	gradient image.Image
-	font *truetype.Font
+	font     *truetype.Font
+	c        *freetype.Context
 )
 
 const (
-	h = 18
+	h  = 18
+	op = 6
+	ip = 4
 )
 
 func init() {
-	log.Println("Initializing png");
+	log.Println("Initializing png")
 
 	fi, _ := os.Open("edge.png")
 	edge, _ = png.Decode(fi)
@@ -72,81 +79,95 @@ func init() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	c = freetype.NewContext()
+	c.SetDPI(72)
+	c.SetFont(font)
+	c.SetFontSize(10)
+}
+
+func getTextOffset(pt raster.Point) int {
+	return int(math.Floor(float64(float32(pt.X)/256 + 0.5)))
+}
+
+func renderString(s string, c *freetype.Context) (image.Image, int) {
+	dst := image.NewRGBA(image.Rect(0, 0, 100, h))
+
+	c.SetDst(dst)
+	c.SetClip(dst.Bounds())
+
+	c.SetSrc(&image.Uniform{shadow})
+	pt := freetype.Pt(0, 13)
+	c.DrawString(s, pt)
+
+	c.SetSrc(image.White)
+
+	pt = freetype.Pt(0, 12)
+	pt, _ = c.DrawString(s, pt)
+
+	return dst, getTextOffset(pt)
 }
 
 func makePngShield(w http.ResponseWriter, d Data) {
 	w.Header().Add("content-type", "image/png")
 
-	mask := image.NewRGBA(image.Rect(0, 0, 100, h))
+	// render text to determine how wide the image has to be
+	// we leave 6 pixels at the start and end, and 3 for each in the middle
+	v, vw := renderString(d.Vendor, c)
+	s, sw := renderString(d.Status, c)
+	imageWidth := op + vw + ip*2 + sw + op
+
+	mask := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
 	draw.Draw(mask, edge.Bounds(), edge, image.ZP, draw.Src)
 
 	sr := image.Rect(2, 0, 3, h)
-	for i := 3; i <= 97; i++ {
+	for i := 3; i <= imageWidth-3; i++ {
 		dp := image.Point{i, 0}
 		r := sr.Sub(sr.Min).Add(dp)
 		draw.Draw(mask, r, edge, sr.Min, draw.Src)
 	}
 
 	sr = image.Rect(0, 0, 1, h)
-	dp := image.Point{99, 0}
+	dp := image.Point{imageWidth - 1, 0}
 	r := sr.Sub(sr.Min).Add(dp)
 	draw.Draw(mask, r, edge, sr.Min, draw.Src)
 
 	sr = image.Rect(1, 0, 2, h)
-	dp = image.Point{98, 0}
+	dp = image.Point{imageWidth - 2, 0}
 	r = sr.Sub(sr.Min).Add(dp)
 	draw.Draw(mask, r, edge, sr.Min, draw.Src)
 
-	img := image.NewRGBA(image.Rect(0, 0, 100, h))
+	img := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
 	draw.Draw(img, img.Bounds(), &image.Uniform{d.Color}, image.ZP, draw.Src)
 
-	rect := image.Rect(0, 0, 50, h)
+	rect := image.Rect(0, 0, op+vw+ip, h)
 	draw.Draw(img, rect, &image.Uniform{Gray}, image.ZP, draw.Src)
 
-	dst := image.NewRGBA(image.Rect(0, 0, 100, h))
+	dst := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
 	draw.DrawMask(dst, dst.Bounds(), img, image.ZP, mask, image.ZP, draw.Over)
 
 	draw.Draw(dst, gradient.Bounds(), gradient, image.ZP, draw.Over)
 
 	gsr := image.Rect(2, 0, 3, h)
-	for i := 3; i <= 97; i++ {
+	for i := 3; i <= imageWidth-3; i++ {
 		dp := image.Point{i, 0}
 		gr := gsr.Sub(gsr.Min).Add(dp)
 		draw.Draw(dst, gr, gradient, gsr.Min, draw.Over)
 	}
 
 	sr = image.Rect(0, 0, 1, h)
-	dp = image.Point{99, 0}
+	dp = image.Point{imageWidth - 1, 0}
 	r = sr.Sub(sr.Min).Add(dp)
 	draw.Draw(dst, r, gradient, sr.Min, draw.Over)
 
 	sr = image.Rect(1, 0, 2, h)
-	dp = image.Point{98, 0}
+	dp = image.Point{imageWidth - 2, 0}
 	r = sr.Sub(sr.Min).Add(dp)
 	draw.Draw(dst, r, gradient, sr.Min, draw.Over)
 
-	c := freetype.NewContext()
-	c.SetDPI(72)
-	c.SetFont(font)
-	c.SetFontSize(10)
-	c.SetDst(dst)
-	c.SetClip(dst.Bounds())
+	draw.Draw(dst, dst.Bounds(), v, image.Point{-op, 0}, draw.Over)
 
-	shadow := color.RGBA{0, 0, 0, 125}
-	c.SetSrc(&image.Uniform{shadow})
-	pt := freetype.Pt(6, 13)
-	c.DrawString(d.Vendor, pt)
-
-	pt = freetype.Pt(53, 13)
-	c.DrawString(d.Status, pt)
-
-	c.SetSrc(image.White)
-
-	pt = freetype.Pt(6, 12)
-	c.DrawString(d.Vendor, pt)
-
-	pt = freetype.Pt(53, 12)
-	c.DrawString(d.Status, pt)
+	draw.Draw(dst, dst.Bounds(), s, image.Point{-(op + vw + ip*2), 0}, draw.Over)
 
 	png.Encode(w, dst)
 }
