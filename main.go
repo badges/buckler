2b495fab66f3effa25ef28ddc79ce513da8717a0
James Bowes <jbowes@repl.ca>
2013-07-29 20:56:02 -0300

    Use filename instead of query string

--- main.go
+++ main.go
@@ -4,15 +4,61 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func buckle(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	log.Printf("Requsted: %v", q)
+var (
+	wsReplacer = strings.NewReplacer("__", "_", "_", " ")
+)
 
-	c := Colors[q["c"][0]]
-	// arg validation goes here
-	d := Data{q["v"][0], q["s"][0], c}
+func shift(s []string) ([]string, string) {
+	return s[1:], s[0]
+}
+
+func buckle(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+
+	if len(parts) != 3 {
+		// error
+	}
+
+	imageName := wsReplacer.Replace(parts[2])
+	imageParts := strings.Split(imageName, "-")
+
+	newParts := []string{}
+	for len(imageParts) > 0 {
+		var head, right string
+		imageParts, head = shift(imageParts)
+
+		// if starts with - append to previous
+		if len(head) == 0 && len(newParts) > 0 {
+			left := ""
+			if len(newParts) > 0 {
+				left = newParts[len(newParts)-1]
+				newParts = newParts[:len(newParts)-1]
+			}
+
+			// trailing -- is going to break color anyways so don't worry
+			imageParts, right = shift(imageParts)
+
+			head = strings.Join([]string{left, right}, "-")
+		}
+
+		newParts = append(newParts, head)
+	}
+
+	if len(newParts) != 3 {
+		// error
+	}
+
+	if !strings.HasSuffix(newParts[2], ".png") {
+		// error
+	}
+
+	c := Colors[newParts[2][0:len(newParts[2])-4]]
+	// validate
+
+	d := Data{newParts[0], newParts[1], c}
 	makePngShield(w, d)
 }
 
@@ -23,7 +69,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	ip := os.Getenv("HOST")
 	port := os.Getenv("PORT")
-	http.HandleFunc("/v1", buckle)
+	http.HandleFunc("/v1/", buckle)
 	http.HandleFunc("/", index)
 	log.Println("Listening on port", port)
 	http.ListenAndServe(ip+":"+port, nil)
