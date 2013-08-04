d37b25da2cec6c347724da520b0ac8ed1000391b
James Bowes <jbowes@repl.ca>
2013-08-04 18:00:24 -0300

    extract common mask building function

--- png.go
+++ png.go
@@ -130,32 +130,33 @@ func renderString(s string, c *freetype.Context) (*image.RGBA, int) {
 	return dst, getTextOffset(pt)
 }
 
+func buildMask(mask *image.RGBA, imageWidth int, tmpl image.Image, imgOp draw.Op) {
+	draw.Draw(mask, tmpl.Bounds(), tmpl, image.ZP, imgOp)
+
+	sr := image.Rect(2, 0, 3, h)
+	for i := 3; i <= imageWidth-3; i++ {
+		dp := image.Point{i, 0}
+		r := sr.Sub(sr.Min).Add(dp)
+		draw.Draw(mask, r, tmpl, sr.Min, imgOp)
+	}
+
+	sr = image.Rect(0, 0, 1, h)
+	dp := image.Point{imageWidth - 1, 0}
+	r := sr.Sub(sr.Min).Add(dp)
+	draw.Draw(mask, r, tmpl, sr.Min, imgOp)
+
+	sr = image.Rect(1, 0, 2, h)
+	dp = image.Point{imageWidth - 2, 0}
+	r = sr.Sub(sr.Min).Add(dp)
+	draw.Draw(mask, r, tmpl, sr.Min, imgOp)
+}
+
 func makePngShield(w io.Writer, d Data) {
 	// render text to determine how wide the image has to be
 	// we leave 6 pixels at the start and end, and 3 for each in the middle
 	v, vw := renderString(d.Vendor, c)
 	s, sw := renderString(d.Status, c)
 	imageWidth := op + vw + ip*2 + sw + op
-
-	mask := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
-	draw.Draw(mask, edge.Bounds(), edge, image.ZP, draw.Src)
-
-	sr := image.Rect(2, 0, 3, h)
-	for i := 3; i <= imageWidth-3; i++ {
-		dp := image.Point{i, 0}
-		r := sr.Sub(sr.Min).Add(dp)
-		draw.Draw(mask, r, edge, sr.Min, draw.Src)
-	}
-
-	sr = image.Rect(0, 0, 1, h)
-	dp := image.Point{imageWidth - 1, 0}
-	r := sr.Sub(sr.Min).Add(dp)
-	draw.Draw(mask, r, edge, sr.Min, draw.Src)
-
-	sr = image.Rect(1, 0, 2, h)
-	dp = image.Point{imageWidth - 2, 0}
-	r = sr.Sub(sr.Min).Add(dp)
-	draw.Draw(mask, r, edge, sr.Min, draw.Src)
 
 	img := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
 	draw.Draw(img, img.Bounds(), &image.Uniform{C: d.Color}, image.ZP, draw.Src)
@@ -164,26 +165,12 @@ func makePngShield(w io.Writer, d Data) {
 	draw.Draw(img, rect, &image.Uniform{C: Grey}, image.ZP, draw.Src)
 
 	dst := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
+
+	mask := image.NewRGBA(image.Rect(0, 0, imageWidth, h))
+	buildMask(mask, imageWidth, edge, draw.Src)
 	draw.DrawMask(dst, dst.Bounds(), img, image.ZP, mask, image.ZP, draw.Over)
 
-	draw.Draw(dst, gradient.Bounds(), gradient, image.ZP, draw.Over)
-
-	gsr := image.Rect(2, 0, 3, h)
-	for i := 3; i <= imageWidth-3; i++ {
-		dp := image.Point{i, 0}
-		gr := gsr.Sub(gsr.Min).Add(dp)
-		draw.Draw(dst, gr, gradient, gsr.Min, draw.Over)
-	}
-
-	sr = image.Rect(0, 0, 1, h)
-	dp = image.Point{imageWidth - 1, 0}
-	r = sr.Sub(sr.Min).Add(dp)
-	draw.Draw(dst, r, gradient, sr.Min, draw.Over)
-
-	sr = image.Rect(1, 0, 2, h)
-	dp = image.Point{imageWidth - 2, 0}
-	r = sr.Sub(sr.Min).Add(dp)
-	draw.Draw(dst, r, gradient, sr.Min, draw.Over)
+	buildMask(dst, imageWidth, gradient, draw.Over)
 
 	draw.Draw(dst, dst.Bounds(), v, image.Point{-op, 0}, draw.Over)
 
